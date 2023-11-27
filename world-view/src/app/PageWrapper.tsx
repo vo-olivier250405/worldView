adf4935c4ece45b08d90ae28078d5fdbb4cc1810
vo_o <vo_o@etna-alternance.net>
2023-11-27 18:19:53 +0100

    [ADD] => Ajout d'animation pour le logo

--- world-view/src/app/PageWrapper.tsx
+++ world-view/src/app/PageWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 export const PageWrapper = ({ children }: { children: any }) => {
   return (
@@ -15,5 +16,32 @@ export const PageWrapper = ({ children }: { children: any }) => {
         </motion.div>
       </AnimatePresence>
     </>
+  );
+};
+
+export const HoverAnimation = ({ children }: { children: any }) => {
+  return (
+    <>
+      <AnimatePresence>
+        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </>
+  );
+};
+
+export const RotateOnClick = ({ children }: { children: any }) => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <motion.div
+      className="block"
+      onClick={() => setIsActive(!isActive)}
+      animate={{
+        rotate: isActive ? 40 : 0,
+      }}
+    >
+      {children}
+    </motion.div>
   );
 };
