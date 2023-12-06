68009fe12dfc24afd27bdc7654ec94332fff6e24
vo_o <vo_o@etna-alternance.net>
2023-12-06 13:00:27 +0100

    [ADD] => Ajout des pays en bordures

--- world-view/src/components/DetailCountry.tsx
+++ world-view/src/components/DetailCountry.tsx
@@ -9,27 +9,6 @@ import { TitleFlag } from "./TitleFlag";
 import { ButtonDisplayEcoDemoData } from "./EcoDemographicData";
 import { ButtonHome } from "./ButtonHome";
 
-export const getDistance = (country1: Country, country2: Country): boolean => {
-  const degToRAd = (degrees: number) => {
-    return degrees * (Math.PI / 180);
-  };
-  const lat1: number = degToRAd(country1.latlng[0]);
-  const lat2: number = degToRAd(country2.latlng[0]);
-  const lng1: number = degToRAd(country1.latlng[1]);
-  const lng2: number = degToRAd(country2.latlng[1]);
-
-  const deltaLat: number = (lat2 - lat1) / 2;
-  const deltaLng: number = (lng2 - lng1) / 2;
-
-  const a: number =
-    Math.sin(deltaLat / 2) ** 2 +
-    Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLng / 2) ** 2;
-  const b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = 6371 * b;
-  console.log(distance);
-  return distance < 10000;
-};
-
 export const DetailCountry = ({ country }: { country: Country }) => {
   return (
     <section>
--- world-view/src/components/GeographicalData.tsx
+++ world-view/src/components/GeographicalData.tsx
@@ -1,8 +1,38 @@
+import { fetchAllCountriesData } from "@/Country/CountryService";
 import { PageWrapper } from "@/app/Animations";
 import { Country } from "@/interfaces/country";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// export const getDistance = (country1: Country, country2: Country): boolean => {
+//   const degToRAd = (degrees: number) => {
+//     return degrees * (Math.PI / 180);
+//   };
+//   const lat1: number = degToRAd(country1.latlng[0]);
+//   const lat2: number = degToRAd(country2.latlng[0]);
+//   const lng1: number = degToRAd(country1.latlng[1]);
+//   const lng2: number = degToRAd(country2.latlng[1]);
+
+//   const deltaLat: number = (lat2 - lat1) / 2;
+//   const deltaLng: number = (lng2 - lng1) / 2;
+
+//   const a: number =
+//     Math.sin(deltaLat / 2) ** 2 +
+//     Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLng / 2) ** 2;
+//   const b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//   const distance = 6371 * b;
+//   console.log(distance);
+//   return distance < 10000;
+// };
 
 export const GeographicalData = ({ country }: { country: Country }) => {
+  const [borders, setBorders] = useState<Country[]>([]);
+
+  useEffect(() => {
+    fetchAllCountriesData().then((response) => {
+      return setBorders(response.data);
+    });
+  }, [country]);
+  console.log(borders, "here");
   return (
     <section className="geographical-data">
       <div className="latling">
@@ -14,7 +44,21 @@ export const GeographicalData = ({ country }: { country: Country }) => {
         )}
       </div>
       {country.area && <h2>{country.area}</h2>}
-      <h1>RAJOUTE LES PAYS EN BORDURE</h1>
+      <div className="borders">
+        <hr />
+        <h2>Pays en bordures</h2>
+        {borders &&
+          borders
+            .filter((element) => country.borders.includes(element.fifa))
+            .map((element: Country) => {
+              return (
+                <div>
+                  <h2>{element.name.common}</h2>
+                </div>
+              );
+            })}
+        <hr />
+      </div>
       {country.region && country.subregion && (
         <h2>
           {country.region}, {country.subregion}
@@ -27,6 +71,7 @@ export const GeographicalData = ({ country }: { country: Country }) => {
 export const ButtonDisplayGeoData = ({ country }: { country: Country }) => {
   const [isClicked, setIsClicked] = useState(true);
   const [isShow, setIsShow] = useState(true);
+
   return (
     <>
       {isShow && (
